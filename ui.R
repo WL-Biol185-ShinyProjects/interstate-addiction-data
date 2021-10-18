5a918954f94537b821a487d72645ef2ea6ad2886
Cecily Stern <sternc23@mail.wlu.edu>
2021-10-18 15:48:05 -0400

    tried to fix dashboard

--- ui.R
+++ ui.R
@@ -11,30 +11,26 @@ dashboardPage(
     #Define content of sidebar - options to go to different pages (US, VA, types of drugs, etc.)
     
     sidebarMenu(
-      menuItem("Background", tabName = "Background", icon = icon("Background")),
-      menuItem("United States", tabName = "United States", icon = icon("United States")),
-      menuItem("Virginia", tabName = "Virginia", icon = icon("Virginia")),
-      menuItem("Data Sources", tabName = "Data Sources", icon=icon("Data Sources"))
+      menuItem("Background", tabName = "Background", icon = icon("dashboard")),
+      menuItem("United States", tabName = "United States", icon = icon("dashboard")),
+      menuItem("Virginia", tabName = "Virginia", icon = icon("dashboard")),
+      menuItem("Data Sources", tabName = "Data Sources", icon = icon("dashboard"))
       )
     ),
 
   dashboardBody(
     tabItems(
-      
-      #First tab content
       tabItem(tabName = "Background",
-              h1("Preliminary Information")),
+              h2("Preliminary Information")),
       
-      #Second tab content
       tabItem(tabName = "United States",
               h2("Addiction in the United States")),
       
-      #Third tab content
       tabItem(tabName = "Virginia",
-              h3("Addiction in Virginia")),
+              h2("Addiction in Virginia")),
       
-      #Fourth tab content
       tabItem(tabName = "Data Sources",
-              h4("Data Sources and Further Information")))
+              h2("Data Sources and Further Information"))
+      )
     )
 )
