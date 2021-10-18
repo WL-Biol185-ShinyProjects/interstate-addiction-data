9fc9c0e90d4a917a6e7541691da233fdbe9c8ea4
Sophia Roche <roches23@mail.wlu.edu>
2021-10-18 16:03:27 -0400

    editing dashboard

--- ui.R
+++ ui.R
@@ -3,10 +3,10 @@
 library(shiny)
 library(shinydashboard)
 
-dashboardPage(
-  dashboardHeader(title = "Interstate Addiction"),
+dashboardPage(skin = "purple",
+  dashboardHeader(title = "Addiction Statistics", titleWidth = 600),
 
-  dashboardSidebar(
+  dashboardSidebar(width = 300,
     
     #Define content of sidebar - options to go to different pages (US, VA, types of drugs, etc.)
     
@@ -14,16 +14,24 @@ dashboardPage(
       menuItem("Background", tabName = "backgroundTab", icon = icon("info")),
       menuItem("United States", tabName = "unitedstatesTab", icon = icon("map-marker-alt")),
       menuItem("Virginia", tabName = "virginiaTab", icon = icon("map-marker-alt")),
-      menuItem("Data Sources", tabName = "datasourcesTab", icon=icon("chart-line"))
+      menuItem("Data Sources", tabName = "datasourcesTab", icon = icon("chart-line"))
       )
     ),
 
+  #Dashboard is created
   dashboardBody(
     tabItems(
       
-      #First tab content
+      #First tab content - contains info about drug addiction in the United States and why we chose this topic
       tabItem(tabName = "backgroundTab",
               h2("Preliminary Information")),
+      
+      h1(strong("Welcome to our drug addiction statistics project"), style = "color: #4DD217", align = "center"),
+      
+      h3(strong("United States Drug Addiction data was compiled from . We used these data to visualize
+                the relationships drug type and death, location and overdose/death rates, and hospital admissions
+                across the United States. We also used these data to hone in on these relationships specifically
+                within the state of Virginia."),
       
       #Second tab content
       tabItem(tabName = "unitedstatesTab",
@@ -37,4 +45,4 @@ dashboardPage(
       tabItem(tabName = "datasourcesTab",
               h2("Data Sources and Further Information")))
     )
-)
+))
