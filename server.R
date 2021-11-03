1a9fde277d8e20b5597581ee1d4a42a8970dce71
Sophia Roche <roches23@mail.wlu.edu>
2021-11-03 16:11:52 -0400

    read major cities' lat/lon values from CityLatLon.csv

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(shinydashboard)
 
-read.csv(CityLatLon, "rt")
+CityLatLon <- read.csv("Dataset-CSV-files/CityLatLon", header = TRUE)
 
 function(input,output, session) {  
   output$CityLatLon <- renderLeaflet({
