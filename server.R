4ffac6fcbbdb89bb1954c154d12781dfe067070c
Sophia Roche <roches23@mail.wlu.edu>
2021-11-03 14:48:02 -0400

    deleted a template I'd put in there awhile ago

--- server.R
+++ server.R
@@ -1,23 +1,4 @@
 library(shiny)
 library(shinydashboard)
 
-function(input,output, session) {
-  
-  lats <- -90:90
-  lons <- -180:180
-  
-  output$worldMap <- renderLeaflet({
-    leaflet() %>%
-      setView(lng = -79.442778, lat = 37.782889, zoom = 5) %>%
-      addTiles()
-  })
-    
-    observe({
-      # note the dummy use of the action button input
-      btn <- input$newButton
-      
-      leafletProxy("worldMap") %>%
-        setView(lng = sample(lons, 1), lat = sample(lats, 1), zoom = 5)
-    })
-    
-  }
+function(input,output, session) {  }
