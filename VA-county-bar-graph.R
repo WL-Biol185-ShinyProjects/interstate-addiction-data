39fd0cb609c9ae4c624adfd6707d8ec2a23b6c7d
Cecily Stern <sternc23@mail.wlu.edu>
2021-11-08 16:15:40 -0500

    deleted VAstatistics2020 file because it wasn't needed, played around with VA-county-bar-graph to make sure other places worked, added underscores to csv file

--- VA-county-bar-graph.R
+++ VA-county-bar-graph.R
@@ -1,3 +1,3 @@
 library(ggplot2)
 
-ggplot(VAstatisticsCleaned, aes(Year, Accomack_County)) + geom_bar(stat = 'identity', fill = "#572EFD") + theme(axis.text.x= element_text(angle = 60, hjust = 1))
+ggplot(VAstatisticsCleaned, aes(Year, Charlottesville_City)) + geom_bar(stat = 'identity', fill = "#572EFD") + theme(axis.text.x= element_text(angle = 60, hjust = 1))
