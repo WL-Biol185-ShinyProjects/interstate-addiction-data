9a1312f041ffc5b0de1aa8f4f807b37e17c7b513
Sophia Roche <roches23@mail.wlu.edu>
2021-11-08 15:07:22 -0500

    ignore these changes, just trying to resolve merge conflict so I can pull

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@
 
 library(shiny)
 library(shinydashboard)
-library(leaflet)
+#library(leaflet)
 
 #CityLatLon <- read.csv("Dataset-CSV-files/CityLatLon", header = TRUE)
 
@@ -17,26 +17,13 @@ dashboardPage(skin = "purple",
       menuItem("Background", tabName = "backgroundTab", icon = icon("info")),
       menuItem("United States", tabName = "unitedstatesTab", icon = icon("map-marker-alt")),
       menuItem("Virginia", tabName = "virginiaTab", icon = icon("map-marked")),
-      menuItem("Data Sources", tabName = "datasourcesTab", icon=icon("server"))
+      menuItem("Data Sources", tabName = "datasourcesTab", icon = icon("server"))
       )
     ),
 
   #Dashboard - main info and data
   dashboardBody(
-    fluidRow(
-      box(title = "Box title", "Box content"),
-      box()),
-        
-    fluidRow(
-      box(title = "Title 1", width = 4, solidHeader = TRUE, status = "primary", "Box content"),
-      box(title = "Title 2", width = 4, solidHeader = TRUE, "Box content"),
-      box(title = "Title 3", width = 4, solidHeader = TRUE, status = "warning", "Box content")),
     
-    fluidRow(
-      box(width = 4, background = "black", "A box with a solid black background"),
-      box(title = "Title 5", width = 4, background = "light-blue", "A box with a solid light-blue background"),
-      box(title = "Title 6", width = 4, background = "maroon", "A box with a solid maroon background")),
-
     tabItems(
       #First tab content - info about drug addiction in the US and why we chose this topic
 
@@ -47,7 +34,10 @@ dashboardPage(skin = "purple",
         h1(strong("Welcome to our drug addiction statistics project"), style = "color: #2C69D2", align = "center"),
         br(),
         h4(("United States Drug addiction data was compiled from -. We used these datasets to visualize the relationships between"), align = "center"),
-        br()
+        br(),
+        box(title = "Title 1", width = 4, solidHeader = TRUE, status = "primary", "Box content"),
+        box(title = "Title 2", width = 4, solidHeader = TRUE, status = "warning", "Box content"),
+        box(title = "Title 3", width = 4, solidHeader = TRUE, status = "warning", "Box content"),
       ),
 
       # Second tab content - info about US cities' drug use, death rates, and other categories
@@ -56,37 +46,21 @@ dashboardPage(skin = "purple",
         tabName = "unitedstatesTab",
         h1(strong("Addiction in the United States")),
         h4("Addiction is a common problem in several of the United States' major cities. Survey data was sourced from the American Addiction Center, based on the percentage of users per city population surveyed.")
-#        box(
-#          selectizeInput(
-#            "select",
-#            label = NULL,
-#            choices = c(
-#              "Select category...",
-#              "Top 5 Cities with Marijuana Use",
-#              "Top 5 Cities with Cocaine Use",
-#              "Top 5 Cities with Heroin Use",
-#              "Top 5 Cities with Meth Use"
-#            ),
-#            selected = "Select category...",
-#            multiple = FALSE,
-#            options = majorCities
-#          )
-#        ),
-#        leafletOutput(CityLatLon)
+
       ),
       
       # Third tab content - info about VA counties' drug use, median incomes, and other categories
 
       tabItem(
         tabName = "virginiaTab",
-        h1(strong("Addiction in Virginia"))
+        h1(strong("Addiction in Virginia")),
       ),
 
       # Fourth tab content
 
       tabItem(
         tabName = "datasourcesTab",
-        h1(strong("Data Sources and Further Information"))
+        h1(strong("Data Sources and Further Information")),
       ),
     )
   )
