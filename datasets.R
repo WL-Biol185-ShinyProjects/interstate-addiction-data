8e39d1431da18b954915afd4c4a61d05be849862
Cecily Stern <sternc23@mail.wlu.edu>
2021-12-01 14:58:59 -0500

    took out datasets that weren't loading

--- datasets.R
+++ datasets.R
@@ -4,9 +4,9 @@ library(ggplot2)
 # Read all data CSVs.
 
 overdosesByState2019 <- read.csv("Dataset-CSV-files/2019 Drug Overdose Deaths Per State.csv", header = TRUE)
-cityLatLon <- read.csv("Dataset-CSV-files/CityLatLon.csv", header = TRUE)
-reportingRates <- read.csv("Dataset-CSV-files/Reporting Rates and Quality Per State.csv", header = TRUE)
-stateDrugUseTrends <- read.csv("Dataset-CSV-files/State Drug Use Trends.csv", header = TRUE)
+cityLatLon <- read.csv("Dataset-CSV-files/CityLatLon", header = TRUE)
+# reportingRates <- read.csv("Dataset-CSV-files/Reporting Rates and Quality Per State.csv", header = TRUE)
+# stateDrugUseTrends <- read.csv("Dataset-CSV-files/State Drug Use Trends.csv", header = TRUE)
 substanceUseEstimatesByCity <- read.csv("Dataset-CSV-files/Substance use estimates by city.csv", header = TRUE)
 surveillanceTrends <- read.csv("Dataset-CSV-files/Surveillance of ER Visit Trends for Overdose Per State.csv", header = TRUE)
 virginiaIncome <- read.csv("Dataset-CSV-files/VA Median Income by County - 2014-2018.csv", header = TRUE)
@@ -36,10 +36,10 @@ VSRRDeathCounts <- read.csv("Dataset-CSV-files/VSRR_Provisional_Drug_Overdose_De
 
 # Top five substances wrangling (in descending order).
 
-marijuanaTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimates$Marijuana),][1:5,]
-cocaineTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimates$Cocaine),][1:5,]
-heroinTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimates$Heroin),][1:5,]
-methTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimates$Meth),][1:5,]
+marijuanaTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimatesByCity$Marijuana),][1:5,]
+cocaineTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimatesByCity$Cocaine),][1:5,]
+heroinTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimatesByCity$Heroin),][1:5,]
+methTopFive <- substanceUseEstimatesByCity[order(-substanceUseEstimatesByCity$Meth),][1:5,]
 
 # Since lat/lon is not in the substances data we need to merge it with the cityLatLon data.
 
